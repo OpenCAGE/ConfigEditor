06085b28c2995b8c1e48dc99eb4a3884a2fc5150
MattFiler <matthew.filer@hotmail.co.uk>
2020-11-28 15:48:31 +0000

    Updated build info

--- AlienConfigEditor/Properties/AssemblyInfo.cs
+++ AlienConfigEditor/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OpenCAGE - Config Editor")]
-[assembly: AssemblyDescription("A tool to edit various configurations in the game Alien: Isolation")]
+[assembly: AssemblyDescription("A tool to edit various configurations in the game Alien: Isolation.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Matt Filer")]
 [assembly: AssemblyProduct("OpenCAGE - Config Editor")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.1.3")]
-[assembly: AssemblyFileVersion("0.8.1.3")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
