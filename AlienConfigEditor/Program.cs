79c4fa7b3ff69eeaba4a3a8ef750215c4736a38c
MattFiler <matthew.filer@hotmail.co.uk>
2023-08-15 20:53:06 +0100

    launch arg

--- AlienConfigEditor/Program.cs
+++ AlienConfigEditor/Program.cs
@@ -23,19 +23,25 @@ namespace AlienConfigEditor
         [STAThread]
         static void Main(string[] args)
         {
+            //Set path to AI
+            if (GetArgument("pathToAI") != null)
+                SharedData.pathToAI = GetArgument("pathToAI");
+            else
+                SharedData.pathToAI = Environment.CurrentDirectory;
+
             //Set paths
-            if (args.Length > 0 && args[0] == "-opencage") for (int i = 1; i < args.Length; i++) SharedData.pathToAI += args[i] + " ";
-            else SharedData.pathToAI = Environment.CurrentDirectory + " ";
-            SharedData.pathToAI = SharedData.pathToAI.Substring(0, SharedData.pathToAI.Length - 1);
             SharedData.pathToWorkingFiles = SharedData.pathToAI + "/DATA/MODTOOLS/WORKING_FILES/";
             SharedData.pathToModsFolder = SharedData.pathToAI + "/DATA/MODS/";
 
             //Verify location
-            if (!File.Exists(SharedData.pathToAI + "/AI.exe")) throw new Exception("This tool was launched incorrectly, or was not placed within the Alien: Isolation directory.");
+            if (!File.Exists(SharedData.pathToAI + "/AI.exe")) 
+                throw new Exception("This tool was launched incorrectly, or was not placed within the Alien: Isolation directory.");
 
             //Create required directories
-            if (!Directory.Exists(SharedData.pathToWorkingFiles)) Directory.CreateDirectory(SharedData.pathToWorkingFiles);
-            if (!Directory.Exists(SharedData.pathToModsFolder)) Directory.CreateDirectory(SharedData.pathToModsFolder);
+            if (!Directory.Exists(SharedData.pathToWorkingFiles)) 
+                Directory.CreateDirectory(SharedData.pathToWorkingFiles);
+            if (!Directory.Exists(SharedData.pathToModsFolder)) 
+                Directory.CreateDirectory(SharedData.pathToModsFolder);
 
             //Add font resources for use
             FontManager.AddFont(Properties.Resources.Isolation_Isolation);
@@ -45,9 +51,20 @@ namespace AlienConfigEditor
             //Run app
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(true);
-            Landing mainLandingPage = new Landing();
-            mainLandingPage.Show();
-            Application.Run();
+            Application.Run(new Landing());
+        }
+
+        static string GetArgument(string name)
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].Contains(name))
+                {
+                    return args[i + 1];
+                }
+            }
+            return null;
         }
     }
 }
